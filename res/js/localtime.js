67097609040e26ad1b8bbf34ac99872459fd4661
Dayan Paez <dayan@openweb-solutions.net>
2015-11-14 11:10:22 -0500

    Bugfix: localtime.js should pad the digits.

--- res/js/localtime.js
+++ res/js/localtime.js
@@ -5,7 +5,13 @@
             return;
         var c = e.textContent.split("@");
         var t = new Date(c[0] + " " + c[1]);
-        var n = (["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"])[t.getMonth()] + " " + t.getDate() + ", " + t.getFullYear() + " @ " + t.getHours() + ":" + t.getMinutes() + ":" + t.getSeconds();
+        var m = t.getMinutes();
+        if (m < 10)
+            m = "0" + m;
+        var s = t.getSeconds();
+        if (s < 10)
+            s = "0" + s;
+        var n = (["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"])[t.getMonth()] + " " + t.getDate() + ", " + t.getFullYear() + " @ " + t.getHours() + ":" + m + ":" + s;
         while (e.childNodes.length > 0)
             e.removeChild(e.childNodes[0]);
         e.appendChild(d.createTextNode(n));
