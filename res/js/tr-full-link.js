c7dcca05d9ea0262b6c9d45d7207a172544c4dec
Dayan Paez <dayan@openweb-solutions.net>
2015-11-14 11:10:33 -0500

    Deprecate tr-full-link.js and more robust tr-full-select.js.

--- res/js/tr-full-link.js
+++ res/js/tr-full-link.js
@@ -2,51 +2,46 @@
  * Provide links to full scores, but only if JS is supported
  */
 
-function extractTeamID(row) {
-    for (var i = 0; i < row.classList.length; i++) {
-        if (/team-[0-9]+/.test(row.classList.item(i)))
-            return row.classList.item(i);
+window.addEventListener("load", function(e) {
+
+    function extractTeamID(row) {
+        for (var i = 0; i < row.classList.length; i++) {
+            if (/team-[0-9]+/.test(row.classList.item(i)))
+                return row.classList.item(i);
+        }
+        return null;
     }
-    return null;
-}
 
-function initFullLink() {
-    var tables = document.getElementsByTagName("table");
-    for (var t = 0; t < tables.length; t++) {
-        if (tables[t].classList.contains("teamranking")) {
-            // Add message before
-            var p = tables[t].previousSibling;
-            if (!p || p.nodeName.toLowerCase() != "p") {
-                p = document.createElement("p");
-                tables[t].parentNode.insertBefore(p, tables[t]);
-            }
-            p.appendChild(document.createTextNode(" Tip: click on team's record to view races."));
+    function initFullLink() {
+        var tables = document.getElementsByTagName("table");
+        for (var t = 0; t < tables.length; t++) {
+            if (tables[t].classList.contains("teamranking")) {
+                // Add message before
+                var p = tables[t].previousSibling;
+                if (!p || p.nodeName.toLowerCase() != "p") {
+                    p = document.createElement("p");
+                    tables[t].parentNode.insertBefore(p, tables[t]);
+                }
+                p.appendChild(document.createTextNode(" Tip: click on team's record to view races."));
 
-            var rows = tables[t].childNodes[1].childNodes;
-            var n, c, href;
-            for (var r = 0; r < rows.length; r++) {
+                var rows = tables[t].childNodes[1].childNodes;
+                var n, c, href;
+                for (var r = 0; r < rows.length; r++) {
 
-                n = extractTeamID(rows[r]);
-                if (n) {
-                    c = rows[r].childNodes[5];
-                    href = document.createElement("a");
-                    href.setAttribute("href", "full-scores/#" + n);
-                    href.setAttribute("title", "Click to view races");
-                    while (c.childNodes.length > 0)
-                        href.appendChild(c.childNodes[0]);
-                    c.appendChild(href);
+                    n = extractTeamID(rows[r]);
+                    if (n) {
+                        c = rows[r].childNodes[5];
+                        href = document.createElement("a");
+                        href.setAttribute("href", "full-scores/#" + n);
+                        href.setAttribute("title", "Click to view races");
+                        while (c.childNodes.length > 0)
+                            href.appendChild(c.childNodes[0]);
+                        c.appendChild(href);
+                    }
                 }
             }
         }
     }
-}
 
-var old = window.onload;
-if (old) {
-    window.onload = function(evt) {
-        old(evt);
-        initFullLink();
-    };
-}
-else
-    window.onload = initFullLink;
+    initFullLink();
+}, false);
