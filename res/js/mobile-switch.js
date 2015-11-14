85843226aca0e3f14abdae640687bb85be159b57
Dayan Paez <dayan@openweb-solutions.net>
2015-11-14 11:10:36 -0500

    Give users the option to switch to mobile view

--- res/js/mobile-switch.js
+++ res/js/mobile-switch.js
@@ -1,13 +1,31 @@
-(function() {
+(function(document) {
     if (window.screen.width < 800) {
-        var c = document.cookie, m = null, s = null;
-        if (/mobile=true/.test(c)) {
+        var enableMobile = function() {
             m = document.createElement("meta");
             m.setAttribute("name", "viewport");
             m.setAttribute("content", "width=device-width,initial-scale=1,maximum-scale=1");
+            m.id = "ms-meta";
             s = document.getElementsByTagName("script")[0];
             s.parentNode.insertBefore(m, s);
-            // provide option to go back?
+        };
+        var disableMobile = function() {
+            var m = document.getElementById("ms-meta");
+            m.parentNode.removeChild(m);
+        };
+        var actOn = function(enable) {
+            if (enable) {
+                enableMobile();
+            }
+            else {
+                disableMobile();
+            }
+            document.cookie = "mobile=" + enable + ";path=/;max-age=3153600";
+        };
+
+        var c = document.cookie, m = null, s = null;
+        var turnedOn = false;
+        if (/mobile=true/.test(c)) {
+            turnedOn = true;
         }
         else if (!/mobile=false/.test(c)) {
             m = document.createElement("script");
@@ -17,5 +35,38 @@
             s = document.getElementsByTagName("script")[0];
             s.parentNode.insertBefore(m, s);
         }
+
+        // Add toggle button
+        var tog = document.createElement("div");
+        tog.id = "ms-toggle";
+        document.getElementById("page-footer").appendChild(tog);
+        var div = document.createElement("div");
+        div.id = "ms-div";
+        tog.appendChild(div);
+
+        var inp = document.createElement("input");
+        inp.id = "ms-button";
+        inp.type = "checkbox";
+        div.appendChild(inp);
+
+        var lab = document.createElement("label");
+        lab.setAttribute("for", "ms-button");
+        div.appendChild(lab);
+
+        var spn = document.createElement("span");
+        spn.id = "ms-button-label";
+        tog.appendChild(spn);
+        spn.appendChild(document.createTextNode("Mobile"));
+
+        inp.addEventListener(
+            "change",
+            function(e) {
+                actOn(inp.checked);
+            },
+            false
+        );
+
+        inp.checked = turnedOn;
+        actOn(turnedOn);
     }
-})();
+})(document);
